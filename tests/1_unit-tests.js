bef4e5f71d74b0e99b0172d37b543c095d175d18
rapreyes reyes <reyesraphaeljohn@gmail.com>
2024-04-10 17:37:23 +0000

    test4

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -25,8 +25,8 @@ suite('Unit Tests', function () {
     // #4
     test('#isTrue, #isNotTrue', function () {
       assert.isTrue(true, 'true is true');
-      assert.fail(!!'double negation', 'Double negation of a truthy value is true');
-      assert.isTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
+      assert.isTrue(!!'double negation', 'Double negation of a truthy value is true');
+      assert.isNotTrue({ value: 'truthy' }, 'Objects are truthy, but are not boolean values');
     });
   });
 
