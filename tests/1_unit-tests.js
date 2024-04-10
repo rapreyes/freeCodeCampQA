4b5c0683c888645ce0c71aae0cb1a712c4de05ed
rapreyes reyes <reyesraphaeljohn@gmail.com>
2024-04-10 18:17:47 +0000

    test1

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -78,7 +78,7 @@ suite('Unit Tests', function () {
     // #10
     test('#approximately', function () {
       assert.approximately(weirdNumbers(0.5), 1, 0.1);
-      assert.approximately(weirdNumbers(0.2), 1, 0.2);
+      assert.approximately(weirdNumbers(0.2), 1, 0);
     });
   });
 
@@ -94,8 +94,8 @@ suite('Unit Tests', function () {
     });
     // #12
     test('Array #include, #notInclude', function () {
-      assert.fail(winterMonths, 'jul', "It's summer in july...");
-      assert.fail(backendLanguages, 'javascript', 'JS is a backend language');
+      assert.notInclude(winterMonths, 'jul', "It's summer in july...");
+      assert.include(backendLanguages, 'javascript', 'JS is a backend language');
     });
   });
 
@@ -107,14 +107,14 @@ suite('Unit Tests', function () {
   suite('Strings', function () {
     // #13
     test('#isString, #isNotString', function () {
-      assert.fail(Math.sin(Math.PI / 4), 'A float is not a string');
-      assert.fail(process.env.PATH, 'An env variable is a string (or undefined)');
-      assert.fail(JSON.stringify({ type: 'object' }), 'JSON is a string');
+      assert.isNotString(Math.sin(Math.PI / 4), 'A float is not a string');
+      assert.isString(process.env.PATH, 'An env variable is a string (or undefined)');
+      assert.isString(JSON.stringify({ type: 'object' }), 'JSON is a string');
     });
     // #14
     test('String #include, #notInclude', function () {
-      assert.fail('Arrow', 'row', "'Arrow' contains 'row'");
-      assert.fail('dart', 'queue', "But 'dart' doesn't contain 'queue'");
+      assert.include('Arrow', 'row', "'Arrow' contains 'row'");
+      assert.notInclude('dart', 'queue', "But 'dart' doesn't contain 'queue'");
     });
     // #15
     test('#match, #notMatch', function () {
